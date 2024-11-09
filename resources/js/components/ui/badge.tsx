05e26d052ce8ab7340be3f3877bd81ba335ad3c3
FearInTheDark <147612664+FearInTheDark@users.noreply.github.com>
2024-11-10 03:55:16 +0700

    User Badges Added

---
+++ resources/js/components/ui/badge.tsx
@@ -0,0 +1,42 @@
+import * as React from "react"
+import {cva, type VariantProps} from "class-variance-authority"
+
+import {cn} from "@/lib/utils"
+
+const badgeVariants = cva(
+    "inline-flex items-center font-inter rounded-md border ml-1 px-2 py-0.2 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+    {
+        variants: {
+            variant: {
+                default:
+                    "border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80",
+                secondary:
+                    "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
+                destructive:
+                    "border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80",
+                outline: "text-foreground",
+                amateur: "border-transparent bg-badge-amateur text-white shadow",
+                beginner: "border-transparent bg-badge-beginner text-white shadow",
+                intermediate: "border-transparent bg-badge-intermediate text-white shadow",
+                admin: "border-transparent bg-badge-admin text-white shadow",
+                dev: "border-transparent bg-gradient-to-l from-rose-500 to-rose-600 text-white shadow",
+            },
+        },
+        defaultVariants: {
+            variant: "default",
+        },
+    }
+)
+
+export interface BadgeProps
+    extends React.HTMLAttributes<HTMLDivElement>,
+        VariantProps<typeof badgeVariants> {
+}
+
+function Badge({className, variant, ...props}: BadgeProps) {
+    return (
+        <div className={cn(badgeVariants({variant}), className)} {...props} />
+    )
+}
+
+export {Badge, badgeVariants}
